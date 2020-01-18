1d90635bccfbeb2f2b97b5f5cc1e7acd5cb87cce
Thierry Kg <t.kaing13@gmail.com>
2020-01-18 05:09:54 +0100

    - Pouvoir vider la table film depuis la route "truncate-film"

--- src/Controller/CinemaController.php
+++ src/Controller/CinemaController.php
@@ -2,11 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Film;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CinemaController extends AbstractController
 {
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
     /**
      * @Route("/", name="index")
      */
@@ -16,5 +26,30 @@ class CinemaController extends AbstractController
             'message' => 'Welcome to CineCity API !',
             'path' => 'src/Controller/CinemaController.php',
         ]);
+    }
+
+    /**
+     * @Route("/truncate/film", name="truncate-film")
+     */
+    public function truncateFilm()
+    {
+        $classMetaDataFilm = $this->manager->getClassMetadata(Film::class);
+        $connection = $this->manager->getConnection();
+
+        try {
+            $dbPlatform = $connection->getDatabasePlatform();
+            $connection->beginTransaction();
+            $connection->query('SET FOREIGN_KEY_CHECKS=0');
+
+            $q = $dbPlatform->getTruncateTableSql($classMetaDataFilm->getTableName());
+
+            $connection->executeUpdate($q);
+            $connection->query('SET FOREIGN_KEY_CHECKS=1');
+            $connection->commit();
+        } catch (\Exception $e) {
+            $connection->rollback();
+        }
+
+        return $this->json(['success' => true]);
     }
 }
